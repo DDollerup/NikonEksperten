07f0a22e35645c2d1e40bdb9ffd28bf4d10956de
PDDaniel <daniel@proper-dog.com>
2017-11-28 13:47:45 +0100

    Add project files.

---
+++ NikonEksperten/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using NikonEksperten.Models;
+using NikonEksperten.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NikonEksperten.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBContext context = new DBContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            List<Category> allCategories = context.CategoryFactory.GetAll();
+            return View(allCategories);
+        }
+
+        // id = CategoryID
+        public ActionResult ProductList(int id = 0)
+        {
+            List<Product> productsByCategoryID = context.ProductFactory.GetAllBy("CategoryID", id);
+
+            return View(productsByCategoryID);
+        }
+    }
+}
